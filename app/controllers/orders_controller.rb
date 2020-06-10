166dd8b2730043f5a0bac758e5fe427f3af9840b
Jocelyn Wang <wangjoc@gmail.com>
2020-06-09 22:08:19 -0700

    completed initital new order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,27 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params) 
+
+    if @order.save 
+      redirect_to order_path(@order.id)
+      flash[:success] = "Successfully added new order: #{view_context.link_to "##{@order.id} #{@order.title}", order_path(@order.id) }"
+      return
+    else 
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def order_params
+    return params.require(:order).permit(:status)
+  end
+
 end
