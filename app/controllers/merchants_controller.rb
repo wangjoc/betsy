f89ba83117bdaeb05c76785acffc1476d79646f7
Hannah Tekié <hannahtekie@gmail.com>
2020-06-10 16:29:44 -0700

    added destroy, dashboard and confirmation method to merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,50 +6,83 @@ class MerchantsController < ApplicationController
   end
 
   def show  
+    @merchant = Merchant.find_by(id: params[:id])
+    check_merchant
   end
 
   def create
     auth_hash = request.env["omniauth.auth"]
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
-    binding.pry
+   
     if merchant
-      flash[:success] = "Logged in as returning user #{merchant.name}"
+      flash[:success] = :success
+      flash[:message] = "Logged in as returning user #{merchant.username}"
     else
       merchant = Merchant.build_from_github(auth_hash)
 
       if merchant.save
-        flash[:success] = "Logged in as new user #{merchant.name}"
+        flash[:success] = :success
+        flash[:message] = "Logged in as new user #{merchant.username}"
       else
-        flash[:error] = "Could not create new user account: #{merchant.errors.messages}"
+        flash[:status] = :error
+        flash[:message] = "Could not create new user account: #{merchant.errors.messages}"
+        flash[:error] = merchant.errors
         return redirect_to root_path
       end
     end
     
     # If we get here, we have a valid user instance
-    session[:merchant_id] = merchant.id
+    session[:user_id] = merchant.id
     return redirect_to root_path
   end
 
-  def logout
-    session[:merchant_id] = nil
-    flash[:success] = "Successfully logged out"
-    redirect_to root_path
-    return
+  def destroy
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to merchant_path
   end
 
-  def current
-    unless @merchant
-      flash[:error] = "You must be logged in to see this page"
-      redirect_to root_path
-      return
-    end
+  def dashboard
+    @merchant = current_merchant
+
+    check_merchant
   end
+
+  def confirmation
+    @merchant = @current_merchant
+    @order = Order.find_by(id: params[:id])
+
+    check_merchant
+  end
+
+  # def logout
+  #   session[:merchant_id] = nil
+  #   flash[:success] = "Successfully logged out"
+  #   redirect_to root_path
+  #   return
+  # end
+
+  # def current
+  #   unless @merchant
+  #     flash[:error] = "You must be logged in to see this page"
+  #     redirect_to root_path
+  #     return
+  #   end
+  # end
 
   private
 
-  def find_merchant
-    @merchant = Merchant.find_by(id: session[:merchant_id])
+  def check_merchant
+    unless @merchant
+      render_404
+    end
   end
+
+  # def find_merchant
+  #   @merchant = Merchant.find_by(id: session[:merchant_id])
+  # end
 
 end
 
