f94b2b78dd94c351538ee16a5ef4e35d19e2796b
Jocelyn Wang <wangjoc@gmail.com>
2020-06-09 20:41:38 -0700

    updated to only have one root path to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homepages#root'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "products#index"
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :products
 end
