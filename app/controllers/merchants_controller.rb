e88e15d3900f2f46cfa2f9168723b3e09bae7d6b
Hannah Tekié <hannahtekie@gmail.com>
2020-06-09 17:57:55 -0700

    initialized index, show, create and destroy for merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,2 +1,25 @@
 class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all 
+  end
+
+  def show
+    @merchants = Merchant.find_by ... something
+  end
+
+  def create
+    #probably do auth here
+  end
+
+  def destroy
+    # session? 
+  end
+
+
+
+
+
+
+
+
 end
