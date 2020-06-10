79f731201084cc4d5aa41882585053107b87f5b6
Halahaddad1 <h.m.haddad@gmail.com>
2020-06-09 19:57:22 -0700

    product model created

---
+++ db/migrate/20200609210555_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.float :price
+      t.string :photo_url
+      t.integer :stock
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+
+PRODUCT_FILE = Rails.root.join('db', 'products_seeds.csv')
+puts "Loading raw media data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.id = row['id']
+  product.name = row['name']
+  product.description= row['description']
+  product.price = row['price']
+  product.photo_url = row['photo_url']
+  product.stock = row['stock']
+
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts "Failed to save product: #{product.inspect}"
+  else
+    puts "Created product: #{product.inspect}"
+  end
+end
+
+puts "Added #{product.count} media records"
+puts "#{product_failures.length} media failed to save"
