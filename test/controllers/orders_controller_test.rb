a08ebde30f0881a0d78a8fd5f262b818820ababf
Jocelyn Wang <wangjoc@gmail.com>
2020-06-11 09:32:22 -0700

    added in test for new order

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "new" do
+    it "responds with success if there nothing in cart" do
+      get new_order_path
+
+      must_respond_with :redirect
+    end
+
+    it "responds with success if there are orders in cart" do
+      populate_cart
+      get new_order_path
+
+      must_respond_with :success
+    end
+  end
 end
