d944896e0eca403ae8b59d1cdb33ec6b2b6c20c4
Halahaddad1 <h.m.haddad@gmail.com>
2020-06-11 00:18:11 -0700

    minor update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :order_items
   belongs_to :merchant
+  has_many :reviews
+  has_and_belongs_to_many :categories
 
   validates :name, presence: true, uniqueness: true
 
@@ -30,6 +32,4 @@ class Product < ApplicationRecord
   end
 
 
-  has_many :reviews
-  has_and_belongs_to_many :categories
 end
