07a816b750a9530036d06e4bd88d1834950804c0
Jocelyn Wang <wangjoc@gmail.com>
2020-06-12 04:20:00 -0700

    added in initial presence validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
   has_many :order_items
+
+  validates :buyer_name, presence: true
+  validates :email_address, presence: true
+  validates :mail_address, presence: true
+  validates :zip_code, presence: true, numericality: true, length: {is: 5}
+  validates :cc_num, presence: true, numericality: true
+  validates :cc_exp, presence: true, numericality: true
 end
