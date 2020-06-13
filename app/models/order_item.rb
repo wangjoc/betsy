b5fa9db93f95e9812d5a33c136aff0ac594bd4b7
Jocelyn Wang <wangjoc@gmail.com>
2020-06-13 06:02:36 -0700

    added in model methods to get OrderItem/Order by order_id and merch_id

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order, optional: true
+
+  def self.items_by_order_merchant(order_id, merch_id)
+    return OrderItem.where(:order_id => order_id).joins(:product).where(:products => {:merchant_id => merch_id})
+  end
 end
