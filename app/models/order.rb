437ab2ad7061ea970c7966f901e9412f195113b1
Jocelyn Wang <wangjoc@gmail.com>
2020-06-14 02:46:38 -0700

    updated validation for email address and requiring order item

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,10 +3,13 @@ class Order < ApplicationRecord
 
   validates :buyer_name, presence: true
   validates :email_address, presence: true
+  validates_format_of :email_address, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/  
   validates :mail_address, presence: true
   validates :zip_code, presence: true, numericality: true, length: {is: 5}
   validates :cc_num, presence: true, numericality: true
   validates :cc_exp, presence: true, numericality: true
+  # validates :order_items, presence: true
+  validates :order_items, :length => { :minimum => 1 }
 
   def self.contains_merchant?(order_id, merch_id)
     # query checks to see if the order contains the merchant
