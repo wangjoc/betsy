064f3e906e0aa54fbf38cba388358670a37cd60c
Leah Hughes <leahwhughes@gmail.com>
2020-06-15 13:31:26 -0700

    organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homepages#root'
 
-  get "/merchants/dashboard", to: "merchants#dashboard", as: "dashboard"
-  get "/orders/receipt", to: "orders#receipt", as: "receipt"
-  get "/orders/confirm", to: "orders#confirm", as: "confirm"
-  patch "/orders/purchase", to: "orders#purchase", as: "purchase"
-
   resources :categories, only: [:show, :new, :create]
-  # I thiiiink we don't need these anymore (Leah added @product to the model of the partial so it's working okay now)
-  #resources :reviews, only: [:new, :create]
   resources :orders, except: [:index]
   resources :merchants, only: [:show, :create]
 
@@ -17,15 +10,23 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
+  # Customer purchase routes - view receipt, confirm order, purchase order
+  get "/orders/receipt", to: "orders#receipt", as: "receipt"
+  get "/orders/confirm", to: "orders#confirm", as: "confirm"
+  patch "/orders/purchase", to: "orders#purchase", as: "purchase"
+
+  # Order managment routes for merchant - dashboard, ship, cancel, complete
+  get "/merchants/dashboard", to: "merchants#dashboard", as: "dashboard"
   patch "/orders/:id/ship", to: "orders#ship", as: "ship"
   patch "/orders/:id/cancel", to: "orders#cancel", as: "cancel"
   patch "/orders/:id/complete", to: "orders#complete", as: "complete"
 
+  # Routes for adding to cart
   patch "/products/:id/add_to_cart", to: "products#add_to_cart", as: "add_to_cart"
   patch "/products/:id/remove_from_cart", to: "products#remove_from_cart", as: "remove_from_cart"
   patch "/products/:id/delete_from_cart", to: "products#delete_from_cart", as: "delete_from_cart"
 
-
+  # GitHub Authorization Routes
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
   post "/logout", to: "merchants#logout", as: "logout"
