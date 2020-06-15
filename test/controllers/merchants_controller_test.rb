f216b89f1d74176237dd9366b8b29482a6d0468b
Jocelyn Wang <wangjoc@gmail.com>
2020-06-14 20:42:03 -0700

    added in show test

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,48 +1,77 @@
-# require "test_helper"
+require "test_helper"
 
-# describe MerchantsController do
-#   # it "does a thing" do
-#   #   value(1+1).must_equal 2
-#   # end
+describe MerchantsController do
+  describe "show" do
+    before do 
+      @merchant_faker = merchants(:faker)
+    end
 
-#   describe "show" do
-#     it "can get the show page" do
-#       merchant = merchants(:hannah)
-#       get merchant_path(merchant)
+    describe "show without login (guest)" do
+      it "can get the show page for valid merchant" do
+        get merchant_path(@merchant_faker.id)
 
-#       must_respond_with :success
-#     end
-#   end
+        must_respond_with :success
+      end
 
-#   describe "dashboard" do 
-#     it "can get the dashboard page" do 
-#       merchant = merchants(:hannah)
-#       perform_login(merchant)
-#       get dashboard_path
+      it "redirect show if invalid merchant" do
+        get merchant_path(-1)
 
-#       must_respond_with :success
-#     end
+        must_respond_with :redirect
+        must_redirect_to products_path
+      end
+    end
 
-#     it "can't get to the dashboard page if not logged in" do 
-#       get dashboard_path 
+    describe "show with login as merchant" do
+      before do 
+        perform_login
+      end
 
-#       must_respond_with :not_found
-#     end
-#   end
+      it "can get the show page for valid merchant" do
+        get merchant_path(@merchant_faker.id)
 
-#   describe "confirmation" do 
-#     it "can get to a confirmation page" do 
-#       merchant = merchants(:hannah)
-#       op = OrderProduct.first
-#       perform_login(merchant)
+        must_respond_with :success
+      end
+
+      it "redirect show if invalid merchant" do
+        get merchant_path(-1)
+
+        must_respond_with :redirect
+        must_redirect_to products_path
+      end
+    end
+
+
+  end
+
+  # describe "dashboard" do 
+  #   it "can get the dashboard page" do 
+  #     merchant = merchants(:hannah)
+  #     perform_login(merchant)
+  #     get dashboard_path
+
+  #     must_respond_with :success
+  #   end
+
+  #   it "can't get to the dashboard page if not logged in" do 
+  #     get dashboard_path 
+
+  #     must_respond_with :not_found
+  #   end
+  # end
+
+  # describe "confirmation" do 
+  #   it "can get to a confirmation page" do 
+  #     merchant = merchants(:hannah)
+  #     op = OrderProduct.first
+  #     perform_login(merchant)
       
-#       get merchant_confirmation_path(op.order_id)
-#       must_respond_with :success
-#     end
+  #     get merchant_confirmation_path(op.order_id)
+  #     must_respond_with :success
+  #   end
 
-#   #   it "should not get into a confirmation page if not logged in" do 
+  #   it "should not get into a confirmation page if not logged in" do 
       
-#   # end
+  # end
 
 
 
@@ -54,5 +83,4 @@
 
 
 
-#   end 
-# end
+end
