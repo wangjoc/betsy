92c7fe1f8e4745166cbb6a92ce6547b7895bd81e
Jocelyn Wang <wangjoc@gmail.com>
2020-06-15 01:44:00 -0700

    added in relationship test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -28,4 +28,34 @@ describe Category do
       end
     end
   end
+
+  describe "relationships" do
+    before do 
+      new_category.save
+      @category = Category.last
+      @product_diaper = products(:diaper)
+      @product_toilet = products(:toilet)
+    end
+
+    it "can have no products" do
+      expect(@category.products).must_be_empty
+    end
+
+    it "can have products" do
+      expect(@category.products.length).must_equal 0
+
+      @category.products << @product_diaper
+      @category.save
+
+      expect(Category.last.products.length).must_equal 1
+      Category.last.products.each do |product|
+        expect(product).must_be_instance_of Product
+      end
+
+      @category.products << @product_toilet
+      @category.save
+
+      expect(Category.last.products.length).must_equal 2
+    end
+  end
 end
