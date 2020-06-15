0716904f997f526d5991099b058061736e69c8ad
Jocelyn Wang <wangjoc@gmail.com>
2020-06-15 01:36:55 -0700

    added in instantiation test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 describe Category do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_category) {
+    Category.new(
+      category: "Weapons"
+    )
+  }
+
+  before do
+    @category_indoor = categories(:indoor)
+    @category_outdoor = categories(:outdoor)
+  end
+
+  describe "instantiation" do
+    it "can be instantiated" do
+      expect(new_category.valid?).must_equal true
+      expect(@category_indoor.valid?).must_equal true
+      expect(@category_outdoor.valid?).must_equal true
+    end
+
+    it "will have the required fields" do
+      new_category.save
+
+      [:category].each do |field|
+        expect(new_category).must_respond_to field
+        expect(@category_indoor).must_respond_to field
+      end
+    end
+  end
 end
