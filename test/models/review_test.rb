2ee22f60e9e3247874bb4bc2b45c4648182de5a1
Leah Hughes <leahwhughes@gmail.com>
2020-06-16 13:49:12 -0700

    adds review model tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 describe Review do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_review) {
+    Review.new(
+      rating: 3,
+      review_text: 'A sample review',
+      product: products(:diaper)
+    )
+  }
+
+  describe 'instantiation' do
+    it 'can be instantiated' do
+      expect(new_review.valid?).must_equal true
+    end
+    
+    it 'has required fields' do
+     expect(new_review).must_respond_to :rating
+     expect(new_review).must_respond_to :review_text
+     expect(new_review).must_respond_to :product
+    end
+  end
+  
+  describe 'relationships' do
+    it 'can access products' do
+      new_review.save
+      
+      expect(new_review.product).must_be_instance_of Product
+      expect(new_review.product.name).must_equal 'Used Diaper'
+    end
+    
+  end
+  
+  describe 'validations' do
+    it 'is not valid without a rating' do
+      new_review.rating = nil
+      new_review.save
+
+      expect(new_review.valid?).must_equal false
+      expect(new_review.errors.messages).must_include :rating
+      expect(new_review.errors.messages[:rating]).must_equal ["can't be blank", "is not a number"]
+    end
+  end
 end
