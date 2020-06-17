94c9316ceb4a5854bcb989e6d9c1aa08bd2fe68e
Halahaddad1 <h.m.haddad@gmail.com>
2020-06-17 12:35:26 -0700

    added stars rating to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,35 @@
 module ApplicationHelper
+
+  def render_rating(rating)
+    return "Be First to Review" unless rating
+    
+    rating = rating.to_i
+
+    filled_star = '<i class="fas fa-star"></i>'
+    empty_star = '<i class="far fa-star"></i>'
+    
+    # make a list of all the filled stars
+    star_list = []
+    until rating == 0
+      star_list << filled_star
+      rating -= 1
+    end
+    
+    # fill out the rest of the list with empty stars
+    until star_list.length == 5
+      star_list << empty_star
+    end
+    
+    # convert star list to string
+    stars = star_list.join("")
+    
+    return (
+      stars.html_safe
+    )
+  end
+
+  
+
   def cart_num_items
     count = 0
 
